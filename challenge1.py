e14c2f30002977505c10868e4f2d5b730f51b86f
Lindsey Anderson <lindsey.anderson@rackspace.com>
2013-03-17 23:15:52 -0500

    updated challenge1.py

--- challenge1.py
+++ challenge1.py
@@ -4,7 +4,6 @@ Cloud Server
 """
 
 import pyrax
-import time
 import re
 import sys
 from imports import auth
@@ -13,20 +12,25 @@ class OpenCloudServer:
 	def __init__(self):
 		self.cs	 = pyrax.cloudservers
 
-	def create_server(self, server_name, server_os, server_size):
+	def create_server(self, server_name, server_os, server_size, image_id=None):
 		self.server_name = server_name
 		self.server_os   = server_os
 		self.server_size = int(server_size)
+		self.image_id    = image_id
 		local_memory = [ memory for memory in self.cs.flavors.list()
 					if memory.ram == self.server_size][0]
-		local_os     = [ image  for image  in self.cs.images.list()
+		if not image_id:
+			local_os     = [ image  for image  in self.cs.images.list()
 					if self.server_os in image.name][0]
+		else:
+			local_os     = [ image for image in self.cs.images.list()
+					if self.image_id in image.id][0]
 		new_server = self.cs.servers.create(self.server_name, local_os.id, local_memory.id)
 		self.server_name = new_server.name
 		self.root_pass   = new_server.adminPass
 		self.server_id   = new_server.id
 		pyrax.utils.wait_until(new_server, "status", ['ACTIVE', 'ERROR'], interval=20, 
-				attempts=60, verbose=True)
+					attempts=60, verbose=True)
 		self.set_current_server_networks()
 	"""
 	Returns public IP address of server
@@ -34,7 +38,7 @@ class OpenCloudServer:
 	def get_server_ip_public(self): 
 		public_network = self.network[u'public']
 		for ip_address in public_network:
-			current_ip = re.findall(r'[0-9]+(>:\.[0-9]+){3}', ip_address)
+			current_ip = re.findall(r'[0-9]+(?:\.[0-9]+){3}', ip_address)
 			if current_ip: return current_ip[0]
 
 	"""
@@ -43,7 +47,7 @@ class OpenCloudServer:
 	def get_server_ip_private(self):
 		public_network = self.network[u'private']
 		for ip_address in public_network:
-			current_ip = re.findall(r'[0-9]+(>:\.[0-9]+){3}', ip_address)
+			current_ip = re.findall(r'[0-9]+(?:\.[0-9]+){3}', ip_address)
 			if current_ip: return current_ip[0]		
 
 	"""
@@ -54,7 +58,7 @@ class OpenCloudServer:
 		cs_ord = pyrax.connect_to_cloudservers(region="DFW")
 		dfw_servers = cs_dfw.servers.list()
 		ord_servers = cs_ord.servers.list()
-		servers = dfw_servers + ord_servers
+		self.servers = dfw_servers + ord_servers
 		for server in servers:
 			if server.name == self.server_name:
 				self.network = server.networks	
