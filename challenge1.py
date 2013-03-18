81119c89a481d4ec8f82551fc063eb8e7cf3aa6c
lindseyanderson <lindseyanderson@dev.justcurl.com>
2013-03-17 22:08:31 -0500

    Fixed

--- challenge1.py
+++ challenge1.py
@@ -4,6 +4,7 @@ Cloud Server
 """
 
 import pyrax
+import time
 import re
 import sys
 from imports import auth
@@ -23,13 +24,10 @@ class OpenCloudServer:
 		new_server = self.cs.servers.create(self.server_name, local_os.id, local_memory.id)
 		self.server_name = new_server.name
 		self.root_pass   = new_server.adminPass
+		self.server_id   = new_server.id
 		pyrax.utils.wait_until(new_server, "status", ['ACTIVE', 'ERROR'], interval=20, 
 				attempts=60, verbose=True)
-		self.network    = new_server.networks
-		self.server_os   = local_os.name
-		self.server_size = local_memory.name
-
-		print self.network
+		self.set_current_server_networks()
 	"""
 	Returns public IP address of server
 	"""
@@ -47,6 +45,19 @@ class OpenCloudServer:
 		for ip_address in public_network:
 			current_ip = re.findall(r'[0-9]+(>:\.[0-9]+){3}', ip_address)
 			if current_ip: return current_ip[0]		
+
+	"""
+	Set Server
+	""" 
+	def set_current_server_networks(self):
+		cs_dfw = pyrax.connect_to_cloudservers(region="ORD")
+		cs_ord = pyrax.connect_to_cloudservers(region="DFW")
+		dfw_servers = cs_dfw.servers.list()
+		ord_servers = cs_ord.servers.list()
+		servers = dfw_servers + ord_servers
+		for server in servers:
+			if server.name == self.server_name:
+				self.network = server.networks	
 	"""
 	Returns server name
 	"""	
@@ -60,7 +71,7 @@ class OpenCloudServer:
 	"""
 	Returns server networks
 	"""
-	def get_server_networks(self): return self.networks
+	def get_server_networks(self): return self.network
 	"""
 	Returns server os
 	"""
@@ -79,8 +90,8 @@ if __name__ == '__main__':
 	server1.create_server(server_name, server_os, server_size)	
 	
 	print "Server Name: " + server1.get_server_name()
-	print "Server Size: " + server1.get_server_size()
-	print "Server OS:   " + server1.get_server_os()
-	print "Server Public IP: " + server1.get_server_ip_public()
-	print "Server Private IP: " + server1.get_server_ip_private()
+	print "Server Size: " + str(server1.get_server_size())
+	print "Server OS:   " + str(server1.get_server_os())
+	print "Server Public IP: " + str(server1.get_server_ip_public())
+	print "Server Private IP: " + str(server1.get_server_ip_private())
 
