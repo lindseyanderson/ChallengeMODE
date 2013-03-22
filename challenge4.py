c182a5015b9d49df95e145a582554bc7f8be96c7
lindseyanderson <lindseyanderson@dev.justcurl.com>
2013-03-21 23:09:29 -0500

    Challenge 4

--- challenge4.py
+++ challenge4.py
@@ -5,5 +5,43 @@ Write a script that uses Cloud DNS to create a new A record when passed a FQDN
 and IP address as arguments
 
 @author: Lindsey Anderson
-@date  : March 17th, 2013
+@date  : March 21st, 2013
 """
+
+import pyrax
+import pyrax.exceptions as exc 
+import pyrax.utils as utils
+
+from imports import auth
+
+if __name__ == '__main__':
+
+	dns = pyrax.cloud_dns
+
+	# accept out input
+	fqdn = raw_input("Please input your domain name as a FQDN: ")
+	ipaddr = raw_input("Please input your target IPv4 address: ")
+
+	# create domain
+	try: 
+		domain = dns.list(name=fdqn)
+	except exc.NotFound:
+		try:
+			domain = dns.create(name=fqdn, emailAddress="ipadmin@stabletransit.net",
+				ttl=300, comment=fqdn)
+		except exc.DomainCreationFailed as ex:
+			print "Domain could not be created:", ex
+		print "Domain created: {0}.".format(str(domain))
+	
+	# create the dict for the record
+	arecord = {"type": "A",
+		   "name": fqdn,
+		   "data": ipaddr,
+  		   "ttl": 300}
+
+	# try to create the record for the domain
+	try:
+		domain.add_records(arecord)
+		print "Record added successfull!"
+	except exc.DomainRecordAdditionFailed as ex:
+		print "Record could not be added:", ex
