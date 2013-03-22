323e9731176a1e69c2182335d181645dc5f85889
lindseyanderson <lindseyanderson@dev.justcurl.com>
2013-03-21 22:24:36 -0500

    Challenge 3

--- challenge3.py
+++ challenge3.py
@@ -9,3 +9,41 @@ appropriately. (Check for invalid paths, etc.)
 @author: Lindsey Anderson
 @date  : March 17, 2013
 """
+
+import pyrax
+import pyrax.exceptions as exc
+import pyrax.utils as utils
+import os
+from imports import auth
+
+if __name__ == "__main__":
+	auth.verify_input()
+
+	container  = raw_input("What container would you like to upload to? ")
+	local_file = raw_input("What local file would you like to upload? ")
+
+	# We need to connect to Cloud Files
+	cf = pyrax.cloudfiles
+	
+	# See if this container already exists
+	# if not, make it.
+	try:
+		cont = cf.get_container(container)
+		print "Container {0} exists.".format(str(cont.name))
+	except exc.NoSuchContainer:	
+		cont = cf.create_container(container)
+		print "Container {0} created.".format(str(cont.name))
+
+	# Make sure we have a valid file	
+ 	try:
+		with open(local_file): pass
+	except IOError:
+		print "This file {0} does not exist.".format(str(cont.name))
+
+	print "Uploading object from {0}.".format(str(local_file))
+	# Store the object in Cloud Files
+	try:
+		object = cf.upload_file(cont.name, local_file)
+		print "Upload completed."
+	except exc.UploadFailed:
+		print "!! An error occured in the upload process, 
