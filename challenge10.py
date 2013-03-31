21601fe989e8b5f1bd3d85bfed6b8a0e183f3f6a
lindseyanderson <lindseyanderson@dev.justcurl.com>
2013-03-30 20:16:53 -0500

    Challenge 10 added

---
+++ challenge10.py
@@ -0,0 +1,113 @@
+#!/usr/bin/python
+"""
+Challenge 10
+- Create 2 servers, supplying a ssh key to be installed at /root/.ssh/authorized_keys.
+- Create a load balancer
+- Add the 2 new servers to the LB
+- Set up LB monitor and custom error page. 
+- Create a DNS record based on a FQDN for the LB VIP. 
+- Write the error page html to a file in cloud files for backup.
+
+"""
+
+import pyrax
+import pyrax.exceptions as exc
+from imports import auth
+from imports import cloudservers
+
+if __name__ == '__main__':
+	
+	auth.verify_input()
+	cs   = pyrax.cloudservers
+	clb  = pyrax.cloud_loadbalancers
+	cdns = pyrax.cloud_dns
+	cf   = pyrax.cloudfiles
+	fqdn_input = "challenge10.justcurl.com"
+
+	image = [img for img in cs.images.list()
+        	if "CentOS 6.3" in img.name][0]
+	size  = [flavor for flavor in cs.flavors.list()
+       		if flavor.ram == 512][0]
+	# This is the data we'll be adding to the new servers
+	public_key_data = """ SSH KEY DATA """
+	files = { "/root/.ssh/authorized_keys" : public_key_data }
+
+	# Create 2 new servers
+	server_list = []
+	for ittid in range(2):
+		server_name = "Challenge10-Server" + str(ittid + 1)
+		print "Creating", server_name
+		new_server = cs.servers.create(server_name, image.id, size.id, files=files)
+		pyrax.utils.wait_until(new_server, "status", ['ACTIVE', 'ERROR'], interval=20, 
+					attempts=60, verbose=True)
+		server_list.append(new_server)
+		print "Server", server_name, "completed."
+			
+	# Create a new Cloud Files container, input our error page
+	container_name = "Challenge10-Container"
+	print "Creating container", container_name
+	container = cf.create_container(container_name)	
+	# Enable the CDN
+	container.make_public(ttl=1200)	
+
+	# Upload custom error page:
+	error_page = """
+	<html>
+	<head><title>Challenge 10 - Custom Error</title></head>
+	<body>
+		Custom Error Page Here.
+	</body>
+	</html>
+	"""
+	error_object = cf.store_object(container, "error.html", error_page)
+	print "Error page has been added to", container.name, "for backup."
+	print "Download here:", container.cdn_uri
+	
+	# Create a Load Balancer
+	lb_name  = "Challenge10-LB"
+	print "Creating", lb_name
+	lb_vip	 = clb.VirtualIP(type="PUBLIC")
+	lb_node0 = clb.Node(address=server[0].networks['private'],
+			port=80, condition="ENABLED")
+	lb_node1 = clb.Node(address=server[1].networks['private'],
+			port=80, condition="ENABLED")
+
+	loadbalancer = create(lb_name, port=80, protocol="HTTP",
+				nodes=[lb_node0, lb_node1], virtual_ips=[lb_vip])
+	pyrax.utils.wait_until(loadbalancer, "status", ['ACTIVE', 'ERROR'], interval=20,
+					attempts=60, verbose=True)
+	print "Load balancer", loadbalancer.name, "created."
+	lb_vip = loadbalancer.virtual_ips[0].address
+	print "Load balancer IP",lb_vip 
+	# Add a health monitor to the load balancer
+	loadbalancer.add_health_monitor(type="CONNECT", delay=5, timeout=30)
+	print "Load balancer health monitor added."
+	# Adding a new error page
+	loadbalancer.errorpage().add(error_page)
+	print "Load balancer error page added."
+
+
+	# Create a FQDN to point to the VIP of the load balancer
+        try:
+                domain = dns.find(name=fqdn_input)
+        except exc.NotFound:
+                try:
+                        domain = dns.create(name=fqdn_input, emailAddress="ipadmin@stabletransit.net",
+                                ttl=300, comment=fqdn_input)
+                except exc.DomainCreationFailed as ex:
+                        print "!! Domain could not be created:", ex
+                        sys.exit(1)
+                print "Domain created: {0}.".format(str(domain.name))
+
+        # create the dict for the record
+        arecord = {"type": "A",
+                   "name": fqdn_input,
+                   "data": lb_vip,
+                   "ttl": 300}
+
+        # try to create the record for the domain
+        try:
+                domain.add_records(arecord)
+                print "A Record added successfull!"
+        except exc.DomainRecordAdditionFailed as ex:
+                print "Record could not be added:", ex	
